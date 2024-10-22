47ba6c9c2df79a107ddcb41a0466af3aa2f5e2aa
하이 <kkm07133@naver.com>
2024-10-22 19:30:55 +0900

    start project

---
+++ src/main/java/com/gdsc/herbhub/HerbHubApplication.java
@@ -0,0 +1,13 @@
+package com.gdsc.herbhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerbHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HerbHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gdsc/herbhub/HerbHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdsc.herbhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerbHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
